fba25c7b95b5a4c9683678db5dbbc95d6bea81eb
oday rami <bak.odey@gmail.com>
2025-08-19 17:57:34 +0300

    task2/r34-09:task2

--- bin/r34_09.dart
+++ bin/r34_09.dart
@@ -1,5 +1,6 @@
 import 'package:task2/task2.dart' as r34_09;
 void main(List<String> arguments) {
+   print(solution1(numbers: [5, 2, 7, 4, 9], count: 22));
  }
 
 // for a = [5,2,7,4,9] and k =5 ,result =3;
