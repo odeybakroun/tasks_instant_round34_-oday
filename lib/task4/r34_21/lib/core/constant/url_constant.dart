17c4023e86ba283eb2f5586ed2e8ec79ee5e5d50
samy mahmoud <samymahmoud2060@gmail.com>
2025-09-30 00:40:05 +0300

    tak4/r34_21:API

--- lib/task4/r34_21/lib/core/constant/url_constant.dart
+++
@@ -1,8 +0,0 @@
-final class UrlConstant {
-  static const String baseURL = "http://dummyjson.com";
-  static const String productsEndPoint = "/products";
-  static const String usersEndPoint = "/posts";
-  static const String postsEndPoint = "/users";
-  
-
-}
--- lib/task4/r34_21/lib/core/network/api_provider.dart
+++
@@ -1,8 +0,0 @@
-
-
-abstract class ApiProvider {
-  Future<dynamic> get(String url);
-  Future<dynamic> Post(String url,{Map<String,dynamic>?body});
-  Future<dynamic> put(String url,{Map<String,dynamic>?body});
-  Future<dynamic> delete(String url);
-}
